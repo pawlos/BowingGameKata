6948bb518b583fd76adde4be79e31207cd140dbd
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2023-07-30 12:20:47 +0200

    Initial version

---
+++ Game/BowlingGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class BowlingGame
+    {
+        public int Score { get; private set; }
+        void Roll(int pins)
+        {
+            Score += pins;
+        }
+    }
+}
